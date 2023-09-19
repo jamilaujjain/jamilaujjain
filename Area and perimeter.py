8b29fe6e408f66a9412ae4051eb86d1b35f85349
jamilaujjain <145117131+jamilaujjain@users.noreply.github.com>
2023-09-19 15:17:58 +0530

    Create Area and perimeter.py

---
+++ Area and perimeter.py
@@ -0,0 +1,15 @@
+radius=int(input("Enter the radius"))
+print("Area of circle is",3.14*radius*radius)
+print("circumference of circle is",2*3.14*radius)
+
+
+side=int(input("Enter the side"))
+print("Area of square is ",side *side)
+print("Perimeter of square is",4*side)
+
+
+a=int(input("Enter the value of a"))
+b=int(input("Enter the value of b"))
+c=int(input("Enter the value of c"))
+print("perimeter of triagle is",a+b+c)
+print("area of triangle is",1/2*a*c)
